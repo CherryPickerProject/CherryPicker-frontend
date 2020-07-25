3574e3b0dd05b89863a6b3f5c50731ce2c6ca899
denzelchung <denzelchung@hotmail.com>
2020-07-25 13:59:29 +0800

    Add Google Analytics support

--- src/layouts/PortalLayout.jsx
+++ src/layouts/PortalLayout.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import { Link } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import styled from 'styled-components';
 import { FaHome } from 'react-icons/fa';
 import theme from '../config/theme';
+
+// Initialize Google Analytics and listen to history
+ReactGA.initialize('UA-173557411-1');
+const browserHistory = createBrowserHistory();
+browserHistory.listen((location) => {
+  ReactGA.pageview(location.pathname + location.search);
+});
 
 const PortalLayoutStyled = styled('div')`
   display: flex;
@@ -80,29 +89,36 @@ const NavTitle = styled.h1`
   color: ${({ color } = {}) => (color ? theme.colours.maroon : theme.colours.black)};
 `;
 
-const PortalLayout = ({ children, pathname }) => (
-  <PortalLayoutStyled>
-    <div className="layout--main">
-      <div className="layout--navbar">
-        <div className="navbar--title">
-          <NavTitle color> Cherry</NavTitle>
-          <NavTitle style={{ marginBottom: '35px' }}>Picker</NavTitle>
+const PortalLayout = ({ children, pathname }) => {
+  useEffect(() => {
+    // Google Analytics log page view
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  return (
+    <PortalLayoutStyled>
+      <div className="layout--main">
+        <div className="layout--navbar">
+          <div className="navbar--title">
+            <NavTitle color> Cherry</NavTitle>
+            <NavTitle style={{ marginBottom: '35px' }}>Picker</NavTitle>
+          </div>
+          <div className="navbar--item">
+            <NavItem selected={pathname === '/about'} to="/about">
+              About us
+            </NavItem>
+            <NavItem selected={pathname === '/bookmarks'} to="/bookmarks">
+              Bookmarks
+            </NavItem>
+            <NavItem selected={pathname === '/'} to="/">
+              <FaHome />
+            </NavItem>
+          </div>
         </div>
-        <div className="navbar--item">
-          <NavItem selected={pathname === '/about'} to="/about">
-            About us
-          </NavItem>
-          <NavItem selected={pathname === '/bookmarks'} to="/bookmarks">
-            Bookmarks
-          </NavItem>
-          <NavItem selected={pathname === '/'} to="/">
-            <FaHome />
-          </NavItem>
-        </div>
+        <div className="layout--children">{children}</div>
       </div>
-      <div className="layout--children">{children}</div>
-    </div>
-  </PortalLayoutStyled>
-);
+    </PortalLayoutStyled>
+  );
+};
 
 export default PortalLayout;
