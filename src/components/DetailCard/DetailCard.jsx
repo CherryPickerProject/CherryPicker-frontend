b0f216e175e5cd41885c4545bd34a36f4973c10a
ooiy0006 <ooiy0006@e.ntu.edu.sg>
2020-07-27 15:13:29 +0800

    remove typo

--- src/components/DetailCard/DetailCard.jsx
+++ src/components/DetailCard/DetailCard.jsx
@@ -2,55 +2,50 @@ import React from 'react';
 import { Segment, Placeholder } from 'semantic-ui-react';
 import SegmentStyle from './DetailCard.styled';
 
-const DetailCard = ({
-  title,
-  location,
-  description,
-  price,
-  imageLink
-} = {}) => (!title ? (
-  <Segment style={SegmentStyle}>
-    <div className="ui items">
-      <div className="item">
-        <div className="image">
-          <Placeholder>
-            <Placeholder.Image />
-          </Placeholder>
-        </div>
-        <div className="content">
-          <Placeholder>
-            <Placeholder.Line length="full" />
-            <Placeholder.Line length="very long" />
-            <Placeholder.Line length="long" />
-            <Placeholder.Line length="medium" />
-          </Placeholder>
+const DetailCard = ({ title, location, description, price, imageLink } = {}) =>
+  !title ? (
+    <Segment style={SegmentStyle}>
+      <div className="ui items">
+        <div className="item">
+          <div className="image">
+            <Placeholder>
+              <Placeholder.Image />
+            </Placeholder>
+          </div>
+          <div className="content">
+            <Placeholder>
+              <Placeholder.Line length="full" />
+              <Placeholder.Line length="very long" />
+              <Placeholder.Line length="long" />
+              <Placeholder.Line length="medium" />
+            </Placeholder>
+          </div>
         </div>
       </div>
-    </div>
-  </Segment>
-) : (
-  <Segment style={SegmentStyle} s>
-    <div className="ui items">
-      <div className="item">
-        <div className="image">
-          <img
-            src={imageLink}
-            alt="description of place"
-            width="200"
-            height="121"
-          />
-        </div>
-        <div className="content">
-          <div className="header">{title}</div>
-          <div className="meta">{location}</div>
-          <div className="description">{description}</div>
-          {price.map((item) => (
-            <div className="ui label">{item}</div>
-          ))}
+    </Segment>
+  ) : (
+    <Segment style={SegmentStyle}>
+      <div className="ui items">
+        <div className="item">
+          <div className="image">
+            <img
+              src={imageLink}
+              alt="description of place"
+              width="200"
+              height="121"
+            />
+          </div>
+          <div className="content">
+            <div className="header">{title}</div>
+            <div className="meta">{location}</div>
+            <div className="description">{description}</div>
+            {price.map((item) => (
+              <div className="ui label">{item}</div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </Segment>
-));
+    </Segment>
+  );
 
 export default DetailCard;
