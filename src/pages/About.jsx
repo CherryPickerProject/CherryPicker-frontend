7644b48d56e18631b7ebba4466b517799004c1a4
denzelchung <denzelchung@hotmail.com>
2020-08-06 13:59:10 +0800

    Add aboutus content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import AboutUs from '../components/AboutUs/AboutUs';
 import PortalLayout from '../layouts/PortalLayout';
 
 const App = ({ location: { pathname } = {} } = {}) => (
-  <PortalLayout pathname={pathname}>TODO: About Page</PortalLayout>
+  <PortalLayout pathname={pathname}>
+    <AboutUs />
+  </PortalLayout>
 );
 
 export default App;
