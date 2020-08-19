a3194e33401b11cd60ecd42cbed5d1e055061396
denzelchung <denzelchung@hotmail.com>
2020-08-19 19:52:43 +0800

    Fix eslint issues

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom'
 import { Grid, Segment } from 'semantic-ui-react';
 import PortalLayout from '../../layouts/PortalLayout';
 import { DetailCard } from '../../components/DetailCard/DetailCard';
 import { PaginationComponent } from '../../components/Pagination/Pagination';
 import style from './Details.styles';
-import { useLocation } from 'react-router-dom'
 import CherryPickerApi from '../../api/CherryPickerApi';
 
 // TODO: Pagination code is not yet done.
 
 const Details = () => {
-
-  const location = useLocation()
+  const location = useLocation();
 
   const [totalDataCount, setTotalDataCount] = useState(0);
   const [data, setData] = useState([]);
@@ -22,20 +21,20 @@ const Details = () => {
       setLoading(true);
       const query = location.state.detail; // User's query passed in here
       const venues = await CherryPickerApi.getVenues(query);
-      setTotalDataCount(venues.totalRecords)
-      setData(venues.data)
+      setTotalDataCount(venues.totalRecords);
+      setData(venues.data);
       setLoading(false);
     }
     fetchVenues();
   }, []);
 
   return (
-    <PortalLayout pathname={"/detail"}>
+    <PortalLayout pathname="/detail">
       <div>
         <div className="row" style={style.filterbarStyle}>
           TODO: Add in the filter components here
         </div>
-        {loading && data.length == 0 ? (
+        {loading && data.length === 0 ? (
           <div>
             <div className="row">
               <Grid centered stackable columns={2}>
@@ -53,23 +52,23 @@ const Details = () => {
             </div>
           </div>
         ) : (
-            <div>
-              <div className="row">
-                <Grid centered stackable columns={2}>
-                  <Grid.Column width={10}>
-                    {data.map((result) => (
-                      <DetailCard {...result} />
-                    ))}
-                  </Grid.Column>
-                  <Grid.Column width={4}>
-                    <Segment style={style.detailCardBorderStyle}>
-                      TODO: MAP
+          <div>
+            <div className="row">
+              <Grid centered stackable columns={2}>
+                <Grid.Column width={10}>
+                  {data.map((result) => (
+                    <DetailCard {...result} />
+                  ))}
+                </Grid.Column>
+                <Grid.Column width={4}>
+                  <Segment style={style.detailCardBorderStyle}>
+                    TODO: MAP
                   </Segment>
-                  </Grid.Column>
-                </Grid>
-              </div>
+                </Grid.Column>
+              </Grid>
             </div>
-          )}
+          </div>
+        )}
         <Grid centered>
           <Grid.Row style={style.paginationStyle}>
             <PaginationComponent totalPages={totalDataCount} />
